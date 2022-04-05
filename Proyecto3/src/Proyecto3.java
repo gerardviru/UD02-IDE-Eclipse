cc0a6030b0a66b4fc91aac5423b3cd64c1d7bc9b
gerar <gerardviru@gmail.com>
2022-04-05 16:06:49 +0200

    Proyecto 3 UD2-IDE

---
+++ Proyecto3/src/Proyecto3.java
@@ -0,0 +1,11 @@
+
+public class Proyecto3 {
+
+	public static void main(String[] args) {
+		/*INDICAMOS UNA FUNCION QUE DETERMINA QUE: SIEMPRE QUE
+		  EL VALOR DE LA VARIABLE I SEA INFERIOR A 6, IMPRIMA EL VALOR*/
+		  	for(int i=0;i<6;i++)
+			System.out.println(i+" ");
+		System.out.println("FIN DE PROGRAMA");
+	}
+}
